0ca6237da8218a2423aa2f34e59b67d33e2c2a9c
Bensas <juanbensa@gmail.com>
2018-09-29 17:59:33 -0300

    Added click detection, Game panel, font

---
+++ Assets/Scripts/ClickHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickHandler : MonoBehaviour {
+    private Camera myCam;
+    public LayerMask collisionLayer;
+    public Spawner spawner;
+    public GamePanel gamePanel;
+
+	// Use this for initialization
+	void Start () {
+        myCam = Camera.main;
+	}
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0)){
+            var mouseRay = myCam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit raycastInfo;
+            if (Physics.Raycast(mouseRay, out raycastInfo, float.MaxValue, collisionLayer)){
+                foreach (Monster monster in spawner.Monsters){
+                    if (Vector3.Distance(monster.transform.position, raycastInfo.point) < 1){
+                        gamePanel.AddScore(GamePanel.KILL_SCORE);
+                        monster.Kill();
+                    }
+                }
+                    
+            }
+
+        }
+
+    }
+}
---
+++ Assets/Scripts/GamePanel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePanel : MonoBehaviour {
+    public const int KILL_SCORE = 30;
+
+    private int score;
+    private int health;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void AddScore(int num){
+        score += num;
+    }
+}
