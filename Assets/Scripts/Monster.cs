c0ae908bdd89b1847e9dd21a45cc4bbd6219e340
jtorreguitar <jtorreguitar@itba.edu.ar>
2018-09-29 19:25:22 -0300

    spawner

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour {
+
+    private Vector3 speed;
+    public GameObject bed;
+
+    public enum STATE { ACTIVE, INACTIVE };
+    STATE state = STATE.INACTIVE;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		if(State == STATE.ACTIVE)
+        {
+            transform.position += speed;
+        }
+	}
+
+    public void spawn()
+    {
+        State = STATE.ACTIVE;
+        transform.position = Spawner.getRandomSpawnPoint();
+        speed = new Vector3(bed.transform.position.x - transform.position.x, bed.transform.position.y - transform.position.y, 0)
+                .normalized;
+    }
+
+    public STATE State { get; private set; }
+}
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,16 +1,69 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private const int FRAMES_PER_SECOND = 60;
+    private const int MAX_MONSTERS_ON_SCREEN = 10;
+    private const int MONSTERS = 20;
+    private const int MINIMUM_SPAWN_TIME = 1 * FRAMES_PER_SECOND;
+    private const int STARTING_SPAWN_TIME = 5 * FRAMES_PER_SECOND;
+    private const int MONSTERS_TILL_LEVEL_UP = 10;
+    private const double DECREASE_IN_SPAWN_TIME = 0.95;
+    private const int SPAWN_POINTS = 3;
+    private double spawnTime = 301;
+    private int monstersOnScreen = 0;
+    private int timeSinceLastSpawn = 0;
+    private int monstersSpawned = 0;
+    private LinkedList<Monster> monsters = new LinkedList<Monster>();
+    private static Vector2[] spawnPoints = { new Vector2(-10.86f, -9.94f), new Vector2(26.79f, -4.1f), new Vector2(0, 0) };
+    public Monster original;
+
+    void Start()
+    {
+        generateMonsters();
+    }
+
+    void Update()
+    {
+        if (monstersOnScreen < MAX_MONSTERS_ON_SCREEN && timeSinceLastSpawn > STARTING_SPAWN_TIME)
+        {
+            Monster newSpawn = getInactiveMonster();
+            if (newSpawn != null)
+            {
+                Debug.Log("spawned");
+                newSpawn.spawn();
+                timeSinceLastSpawn = 0;
+                monstersSpawned++;
+            }
+        }
+        else
+            timeSinceLastSpawn++;
+        if (monstersSpawned % MONSTERS_TILL_LEVEL_UP == 0)
+            spawnTime *= DECREASE_IN_SPAWN_TIME;
+    }
+
+    private Monster getInactiveMonster()
+    {
+        foreach (Monster m in monsters)
+        {
+            if (m.State == Monster.STATE.INACTIVE)
+                return m;
+        }
+        return null;
+    }
+
+    private void generateMonsters()
+    {
+        for (int i = 0; i < MONSTERS; i++)
+            monsters.AddFirst(Instantiate(original, new Vector3(20,-25), Quaternion.identity));
+    }
+
+    public static Vector2 getRandomSpawnPoint()
+    {
+        System.Random random = new System.Random();
+        return spawnPoints[random.Next(3)];
+    }
 }
