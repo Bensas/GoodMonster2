1d86ab0609ae97e10b64f629e407336456e568da
jtorreguitar <jtorreguitar@itba.edu.ar>
2018-09-29 20:55:58 -0300

    speed factor now a thing

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -6,36 +6,39 @@ public class Monster : MonoBehaviour {
 
     private Vector3 speed;
     private float speedFactor;
+    public static int MonstersOnScreen = 0;
     public GameObject bed;
 
     public enum STATE { ACTIVE, INACTIVE };
 
-	void Start ()
+    void Start()
     {
         State = STATE.INACTIVE;
-        speedFactor = 0.5f;
+        speedFactor = 1f/(new System.Random().Next(12,25));
     }
-	
-	void Update ()
+
+    void Update()
     {
-		if(State == STATE.ACTIVE)
+        if (State == STATE.ACTIVE)
         {
             Vector3 finalSpeed = new Vector3(speed.x * speedFactor, speed.y * speedFactor, speed.z * speedFactor);
             transform.position += finalSpeed;
         }
-	}
+    }
 
-    public void Spawn()
+    public void Spawn(Vector2 spawnPosition)
     {
         State = STATE.ACTIVE;
-        transform.position = Spawner.GetRandomSpawnPoint();
+        transform.position = spawnPosition;
         speed = Vector3.Normalize(new Vector3(bed.transform.position.x - transform.position.x, bed.transform.position.y - transform.position.y, 0));
+        MonstersOnScreen++;
     }
 
     public void Kill()
     {
         State = STATE.INACTIVE;
         transform.position = Spawner.OUT_OF_MAP_POSITION;
+        MonstersOnScreen--;
     }
 
     public STATE State { get; private set; }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -10,8 +10,8 @@ public class Spawner : MonoBehaviour {
     private const int MONSTERS = 20;
     private const int MINIMUM_SPAWN_TIME = 1 * FRAMES_PER_SECOND;
     private const int STARTING_SPAWN_TIME = 5 * FRAMES_PER_SECOND;
-    private const int MONSTERS_TILL_LEVEL_UP = 10;
-    private const double DECREASE_IN_SPAWN_TIME = 0.95;
+    private const int MONSTERS_TILL_LEVEL_UP = 1;
+    private const double DECREASE_IN_SPAWN_TIME = 0.5;
     private const int SPAWN_POINTS = 3;
     public static readonly Vector3 OUT_OF_MAP_POSITION = new Vector3(20, -25);
     private double spawnTime = STARTING_SPAWN_TIME;
@@ -28,12 +28,12 @@ public class Spawner : MonoBehaviour {
 
     void Update()
     {
-        if (monstersOnScreen < MAX_MONSTERS_ON_SCREEN && timeSinceLastSpawn > STARTING_SPAWN_TIME)
+        if (Monster.MonstersOnScreen < MAX_MONSTERS_ON_SCREEN && timeSinceLastSpawn > STARTING_SPAWN_TIME)
         {
             Monster newSpawn = GetInactiveMonster();
             if (newSpawn != null)
             {
-                newSpawn.Spawn();
+                newSpawn.Spawn(GetRandomSpawnPoint());
                 timeSinceLastSpawn = 0;
                 monstersSpawned++;
             }
@@ -61,7 +61,7 @@ public class Spawner : MonoBehaviour {
             Monsters.Add(Instantiate(original, OUT_OF_MAP_POSITION, Quaternion.identity));
     }
 
-    public static Vector2 GetRandomSpawnPoint()
+    public Vector2 GetRandomSpawnPoint()
     {
         System.Random random = new System.Random();
         return spawnPoints[random.Next(3)];
