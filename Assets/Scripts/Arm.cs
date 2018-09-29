58f83c2cd9ca1941b4c7e2bd966b7f89cad4aaad
Bensas <juanbensa@gmail.com>
2018-09-29 15:52:53 -0300

    Created Hand and arm

--- Assets/Scripts/Arm.cs
+++ Assets/Scripts/Arm.cs
@@ -3,14 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Arm : MonoBehaviour {
+    private Camera myCam;
+    private LineRenderer lineRenderer;
+    public GameObject source, destination;
 
 	// Use this for initialization
 	void Start () {
-		
+        myCam = Camera.main;
+        lineRenderer = GetComponent<LineRenderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        lineRenderer.SetPosition(0, source.transform.position);
+        lineRenderer.SetPosition(1, destination.transform.position);
+        //Debug.Log(-Vector2.SignedAngle((Vector2)pivot.transform.position, myCam.ScreenToWorldPoint(Input.mousePosition)));
+        //float rotation = Vector2.SignedAngle((Vector2)pivot.transform.position, myCam.ScreenToWorldPoint(Input.mousePosition));
+        //float rotation = 
+
+        //var mouseRay = myCam.ScreenPointToRay(Input.mousePosition);
+        //RaycastHit raycastInfo;
+        //if (Physics.Raycast(mouseRay, out raycastInfo, float.MaxValue, collisionLayer)){
+        //    pivot.transform.LookAt(raycastInfo.point);
+        //    pivot.transform.eulerAngles = new Vector3(0, 0, pivot.transform.eulerAngles.z);
+        //}
+        //transform.localScale = new Vector3(1, Vector2.Distance((Vector2)pivot.transform.position, myCam.ScreenToWorldPoint(Input.mousePosition)), 1);
+
+    }
 }
---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour {
+    private Camera myCam;
+    public LayerMask collisionLayer;
+
+	// Use this for initialization
+	void Start () {
+        myCam = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var mouseRay = myCam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit raycastInfo;
+        if (Physics.Raycast(mouseRay, out raycastInfo, float.MaxValue, collisionLayer))
+            transform.position = raycastInfo.point;
+	}
+}
